459de81b8bbe5b0fd39a23dd771ce0f46827c00d
Christopher Kaag <christopher.kaag@gmail.com>
2021-03-05 21:00:10 +0100

    feat: can now deal with subfolders in enabled directories (more useful for most apps)

--- modules/doc-lib-host/src/main/java/com/github/ckaag/liferay/doclib/host/config/HostDirectoriesSource.java
+++ modules/doc-lib-host/src/main/java/com/github/ckaag/liferay/doclib/host/config/HostDirectoriesSource.java
@@ -1,9 +1,11 @@
 package com.github.ckaag.liferay.doclib.host.config;
 
+import java.util.List;
+
 import com.liferay.document.library.kernel.model.DLFileEntry;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 
 public interface HostDirectoriesSource {
-    DLFileEntry getFileOrNull(long companyId, String folderName, String fileName) throws PortalException;
+    DLFileEntry getFileOrNull(long companyId, String folderName, String fileName, List<String> subfolder) throws PortalException;
 }
